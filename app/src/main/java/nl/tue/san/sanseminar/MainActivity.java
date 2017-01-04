ffe3c24062da9517ecaa9858011dfd8560505a5b
Maurice Knoop <mpjwknoop@gmail.com>
2017-01-04 16:44:58 +0100

    Define some structure to allow multiple Fragments

--- app/src/main/java/nl/tue/san/sanseminar/MainActivity.java
+++ app/src/main/java/nl/tue/san/sanseminar/MainActivity.java
@@ -2,20 +2,23 @@ package nl.tue.san.sanseminar;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,23 +26,17 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        this.navigationView = (NavigationView) findViewById(R.id.nav_view);
+        this.navigationView.setNavigationItemSelectedListener(this);
+
+        // Set the default item to be selected
+        this.setContentType(DEFAULT_CONTENT_TYPE);
     }
 
     @Override
@@ -74,22 +71,148 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        final int id = item.getItemId();
+    /**
+     * Given a resource id for a button in the navigation drawer, this method determines which
+     * ContentType should be displayed. If
+     * @param id The resource id.
+     * @return The ContentType instance. This is never null.
+     * @throws UnknownNavigationResourceException If the given resource id can't be translated to a
+     * ContentType.
+     */
+    private static ContentType contentTypeForNavigationResource(int id){
+        for(ContentType type : ContentType.values())
+            if(type.navigationResource == id)
+                return type;
+        throw new UnknownNavigationResourceException(id);
+    }
+    /**
+     * Translate a ContentType into a corresponding Fragment instance.
+     * @param contentType The ContentType for which to construct a fragment. Can't be null.
+     * @return A Fragment instance that corresponds to the given content type.
+     */
+    private static Fragment constructFragmentForContentType(ContentType contentType){
 
-        if (id == R.id.nav_home) {
+        if(contentType == null)
+            throw new IllegalArgumentException("ContentType can't be null");
 
-        } else if (id == R.id.nav_task_set) {
+        return HomeFragment.newInstance(contentType.name());
+    }
 
-        } else if (id == R.id.nav_visualization) {
 
+    /**
+     * Indicates the ContentType that is currently being displayed
+     */
+    private ContentType currentContentType;
+
+    private static final ContentType DEFAULT_CONTENT_TYPE = ContentType.HOME;
+    /**
+     * Updates the current content type that is being displayed. If the given content type differs
+     * from the currently displayed content type, then the fragment that is currently displayed is
+     * replaced.
+     * @param contentType The content type to display. Can't be null. If it is equal to the current
+     *                    content type then calling this method will have no effect.
+     */
+    private synchronized void setContentType(ContentType contentType) {
+
+        // Check for null
+        if (contentType == null)
+            throw new IllegalArgumentException("Given ContentType can't be null");
+
+        // Check whether we're changing
+        if (this.currentContentType == contentType)
+            return;
+
+        // update the fragment
+        Fragment fragment = constructFragmentForContentType(contentType);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.content_main, fragment)
+                .commit();
+
+        // Start to update all other properties
+        final ActionBar actionBar = this.getSupportActionBar();
+        final Navigatable.Properties properties = fragment instanceof Navigatable ? ((Navigatable) fragment).getProperties() : null;
+        final FloatingActionButton fab = (FloatingActionButton) this.findViewById(R.id.fab);
+
+
+        // Apply the title properties to the action bar, if applicable
+        if (actionBar != null){
+            if (properties != null && properties.usesTitle())
+                actionBar.setTitle(properties.getTitle(this));
+            else
+                actionBar.setTitle("SAN Seminar");
         }
 
+        // Show and handle the Floating Action Button, if applicable
+        if(properties != null && properties.usesFloatingActionButton()){
+            fab.setVisibility(View.VISIBLE);
+            fab.setOnClickListener(properties.getFabClickListener());
+            fab.setImageResource(properties.getFabIconResource());
+        } else {
+            fab.setVisibility(View.GONE);
+            fab.setOnClickListener(null);
+        }
+
+        // Update the current content type, and reflect this in the navigation drawer.
+        this.currentContentType = contentType;
+        this.navigationView.setCheckedItem(this.currentContentType.navigationResource);
+
+    }
+
+
+
+
+
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        try {
+            this.setContentType(contentTypeForNavigationResource(item.getItemId()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Unknown navigation item", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        // Fragment is updated, close drawer and indicate that it is handled.
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    /**
+     * Enumeration indicating the three different types of content that can be shown on the screen:
+     * <ul>
+     *     <li>HOME, indicating the home screen of the app</li>
+     *     <li>TASK_SET, indicating that an overview of all tasks is shown</li>
+     *     <li>VISUALIZATION, indicating that an overview of all visualization rules regarding a
+     *     schedule are shown.</li>
+     * </ul>
+     */
+    private enum ContentType {
+        HOME(R.id.nav_home),
+        TASK_SET(R.id.nav_task_set),
+        VISUALIZATION(R.id.nav_visualization);
+
+        /**
+         * The identifier of the button in the navigation drawer that leads to a ContentType being
+         * selected.
+         */
+        public final int navigationResource;
+
+        ContentType(int navigationResource) {
+            this.navigationResource = navigationResource;
+        }
+    }
+
+    /**
+     * Exception class to indicate that for the given resource we do not know how to navigate elsewhere
+     */
+    private static final class UnknownNavigationResourceException extends RuntimeException{
+        UnknownNavigationResourceException(int resourceId){
+            super("The resource with id 0x"+Integer.toHexString(resourceId)+"("+resourceId+") is not known as a navigation resource");
+        }
     }
 }
---
+++ app/src/main/java/nl/tue/san/sanseminar/Navigatable.java
@@ -0,0 +1,120 @@
+package nl.tue.san.sanseminar;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+/**
+ * Created by Maurice on 4-1-2017.
+ *
+ */
+
+public interface Navigatable {
+
+    /**
+     * Gets the properties for navigation purposes.
+     */
+    Properties getProperties();
+
+    /**
+     * Class representing the properties that are required for Fragments to be used in MainActivity
+     */
+    class Properties {
+
+        private final int fabIconResource;
+        private final View.OnClickListener fabClickListener;
+        private final Object title;
+
+        private static final int NONE = -1;
+
+        public boolean usesFloatingActionButton(){
+            return this.fabIconResource != NONE && this.fabClickListener != null;
+        }
+
+        public int getFabIconResource(){
+            return this.fabIconResource;
+        }
+
+        public View.OnClickListener getFabClickListener(){
+            return this.fabClickListener;
+        }
+
+        public boolean usesTitle(){
+            return this.title != null;
+        }
+
+
+
+        private Properties(int fabIconResource, View.OnClickListener fabClickListener, Object title) {
+            this.fabIconResource = fabIconResource;
+            this.fabClickListener = fabClickListener;
+            this.title = title;
+        }
+
+        public String getTitle(Context context) {
+            if(title instanceof String)
+                return (String) title;
+            else
+                return context.getString((int)title);
+        }
+
+
+        public static class Builder{
+
+            private int fabIconResource = Properties.NONE;
+            private View.OnClickListener fabClickListener = null;
+
+            private Object title = null;
+
+
+            /**
+             * Use the given resource as the icon on the FloatingActionButton
+             * @param fabIconResource
+             * @return This builder for chaining.
+             */
+            public Builder useFabIcon(int fabIconResource){
+                this.fabIconResource = fabIconResource;
+                return this;
+            }
+
+            /**
+             * The OnClickListener to use to handle click events on the Floating Action Button
+             * @param listener The listener to use to handle click events on the Floating Action Button.
+             * @return This builder for chaining.
+             */
+            public Builder useFabHandler(View.OnClickListener listener){
+                this.fabClickListener = listener;
+                return this;
+            }
+
+            /**
+             * Use the given text as title.
+             * @param title The text to use as a title.
+             * @return This builder for chaining.
+             */
+            public Builder useTitle(String title){
+                this.title = title;
+                return this;
+            }
+
+            /**
+             * Use the given resource as title.
+             * @param title The resource to use as a title.
+             * @return This builder for chaining.
+             */
+            public Builder useTitle(int title){
+                this.title = title;
+                return this;
+            }
+
+
+
+            public Properties build(Fragment fragment){
+
+                return new Properties(this.fabIconResource, this.fabClickListener, this.title);
+            }
+
+        }
+    }
+
+}
