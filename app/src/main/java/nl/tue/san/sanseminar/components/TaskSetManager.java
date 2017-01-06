4d01f5053c7219ba791f6fd9950e320b40192a17
Maurice Knoop <mpjwknoop@gmail.com>
2017-01-06 14:14:37 +0100

    Let TaskSetManager write to files

--- app/src/main/java/nl/tue/san/sanseminar/components/TaskSetManager.java
+++ app/src/main/java/nl/tue/san/sanseminar/components/TaskSetManager.java
@@ -1,5 +1,13 @@
 package nl.tue.san.sanseminar.components;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,9 +16,71 @@ import nl.tue.san.sanseminar.concurrent.ReadWriteSafeObject;
 /**
  * Created by Maurice on 6-1-2017.
  */
-public class TaskSetManager extends ReadWriteSafeObject{
+public final class TaskSetManager extends ReadWriteSafeObject{
 
-    private TaskSetManager() {
+    private final static TaskSetManager instance = new TaskSetManager();
+
+    private static final File root = null;
+
+    private TaskSetManager() { }
+
+
+    /**
+     * Writes the given TaskSet to a file within the root directory as a JSONObject. It uses the
+     * filename as specified by {@link TaskSetManager#fileNameFor(TaskSet)}.
+     * @param taskSet The TaskSet to write
+     * @return Whether the file was successfully written to.
+     * @throws JSONException If the given TaskSet could not be translated to JSON.
+     */
+    private void writeToFile(final TaskSet taskSet) throws IOException, JSONException {
+
+        if(root == null)
+            throw new IOException("Not allowed to access directory");
+
+        final JSONObject translation = TaskSetIO.toJSON(taskSet);
+
+        // Ensure that we have the write lock to write to the directory.
+        IOException exception = this.writeOp(new Operation<IOException>() {
+            @Override
+            public IOException perform() {
+                try {
+                    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(root,fileNameFor(taskSet)))));
+                    writer.write(translation.toString());
+                    writer.close();
+                    return null;
+                } catch (IOException e) {
+                    return e;
+                }
+            }
+        });
+
+        // Throw the returned IOException
+        if(exception != null)
+            throw exception;
+    }
+
+    /**
+     * Get the name of the file that should be used to store a TaskSet that has the given name.
+     * The name of the file is the name of the TaskSet followed by the extension "taskset".
+     * A shorthand method exists to get the file name for a given TaskSet as
+     * {@link #fileNameFor(TaskSet)}.
+     * @param taskSetName The name of the TaskSet to store.
+     * @return The filename that should be used to store a TaskSet.
+     * @see #fileNameFor(TaskSet)
+     */
+    private static String fileNameFor(String taskSetName){
+        return taskSetName+".taskset";
+    }
+
+    /**
+     * Get the name of the file that should be used to store the given TaskSet. This equals calling
+     * {@link #fileNameFor(String)} with as argument {@code taskSet.getName()}.
+     * @param taskSet The TaskSet to store.
+     * @return The filename that should be used to store a TaskSet.
+     * @see TaskSetManager#fileNameFor(String)
+     */
+    private static String fileNameFor(TaskSet taskSet){
+        return fileNameFor(taskSet.getName());
     }
 
     /**
@@ -19,7 +89,14 @@ public class TaskSetManager extends ReadWriteSafeObject{
      * @param taskSet The TaskSet to save.
      */
     public static boolean save(TaskSet taskSet){
-        return false;
+
+        try {
+            instance.writeToFile(taskSet);
+            return true;
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     /**
